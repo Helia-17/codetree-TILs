0bbe523a01d4664602aa6700d5dce6c94f1da7b3
김지윤(Jiyun Kim) <76979903+Helia-17@users.noreply.github.com>
2024-02-05 17:51:53 +0900

    Title: 최소공배수 구하기, Time: 173ms, Memory: 14MB, Status: Passed - CodeTree

--- 240205/최소공배수 구하기/find-the-least-common-multiple.js
+++ 240205/최소공배수 구하기/find-the-least-common-multiple.js
@@ -5,7 +5,8 @@ getLCM(n, m);
 
 function getGCD (n, m) {
     let lcm = 1
-    for (let i = 2; i < Math.min(n, m); i++) {
+    // [!] n, m이 같은 경우, i < Math.min(n. m)이 아니라 i <= Math.min(n. m)이어야 한다.
+    for (let i = 2; i <= Math.min(n, m); i++) {
         if (n % i === 0 && m % i === 0) {
             lcm = i
         }
