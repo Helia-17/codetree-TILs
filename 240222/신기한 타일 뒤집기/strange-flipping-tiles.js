145d6aa6c076269114d376ca65e8d0514b38492f
김지윤(Jiyun Kim) <76979903+Helia-17@users.noreply.github.com>
2024-02-22 10:55:20 +0900

    Title: 신기한 타일 뒤집기, Time: 410ms, Memory: 45MB, Status: Passed - CodeTree

---
+++ 240222/신기한 타일 뒤집기/strange-flipping-tiles.js
@@ -0,0 +1,38 @@
+/** 
+ *  1 R, 1 L의 경우 현재 타일의 색상만 바꾸기 때문에 항상 번갈아 L, R이 나온다는 보장이 없다.
+ *  따라서 흰색과 검은색으로 칠해지는 횟수를 따로 구해야 한다.
+ *  또한 구간 문제가 아니므로 기본 for문을 사용한다.
+ * */ 
+
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split("\n")
+let n = +input[0]
+let orderList = input.slice(1).map(order => order.trim().split(" "))
+function solution() {
+    // 한쪽으로만 100개씩 1000번 갈 경우를 고려해 길이 설정
+    let list = Array.from(Array(200001), () => Array(2).fill(0))
+    let cur = 100000
+    // 각 명령을 순회, 명령 내에서 각 영역을 순회
+    for (let [x, d] of orderList) {
+        x = Number(x)
+        if (d === "L"){
+            for (let i = 0; i < x; i++) {
+                list[cur - i][0] += 1
+                list[cur - i][1] = "W"
+            }
+            cur -= x - 1
+        } else {
+            for (let j = 0; j < x ; j++) {
+                list[cur + j][0] += 1
+                list[cur + j][1] = "B"
+            }
+            cur += x - 1
+        }
+    }
+
+    let white = list.filter(p => p[1] === 'W').length
+    let black = list.filter(p => p[1] === 'B').length
+    console.log([white, black].join(" "))
+}
+
+solution()
