9df41bff03c77770e588a22094ea498f51fd0ab0
김지윤(Jiyun Kim) <76979903+Helia-17@users.noreply.github.com>
2024-02-28 23:49:34 +0900

    Title: 반복 출력하기, Time: 197ms, Memory: 47MB, Status: Passed - CodeTree

---
+++ 240228/반복 출력하기/repeated-output.js
@@ -0,0 +1,8 @@
+const fs = require("fs");
+let input = +fs.readFileSync(0).toString().trim()
+function solution () {
+    for (i = 0; i < input; i++ ){
+        console.log('12345^&*()_')
+    }
+}
+solution()
