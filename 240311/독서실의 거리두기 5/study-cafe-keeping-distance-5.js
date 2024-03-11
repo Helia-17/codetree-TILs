85c192f09bb3838fa1dcb46026780ae06369ce08
김지윤(Jiyun Kim) <76979903+Helia-17@users.noreply.github.com>
2024-03-11 23:40:19 +0900

    Title: 독서실의 거리두기 5, Time: 174ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240311/독서실의 거리두기 5/study-cafe-keeping-distance-5.js
+++ 240311/독서실의 거리두기 5/study-cafe-keeping-distance-5.js
@@ -4,17 +4,22 @@ let n = input[0].split(" ").map(Number)
 let list = input[1]
 
 function solution() {
-    let min = 20
-    let cnt = 0
-    for (let seat of list) {
-        if (seat === '1') {
-            if (cnt !== 0 && cnt + 1 < min) min = cnt + 1
-            cnt = 0
-        } else {
-            cnt += 1
+    let max = 0
+    for (let i = 0; i < n; i++) {
+        let cnt = 0
+        let min = 20
+        if (list[i] === '1') continue
+        for (let seat of list) {
+            if (seat === '1') {
+                if (cnt !== 0 && cnt < min) min = cnt
+                cnt = 0
+            } else {
+                cnt += 1
 
+            }
         }
+        if (min > max) max = min
     }
-    console.log(min)
+    console.log(max)
 }
 solution()
