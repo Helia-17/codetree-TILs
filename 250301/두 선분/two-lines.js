f12f15c5b2707711367a0bdd5d155b138256dd65
김지윤(Jiyun Kim) <76979903+Helia-17@users.noreply.github.com>
2025-03-01 16:20:26 +0900

    Title: 두 선분, Time: 185ms, Memory: 15MB, Status: Passed - CodeTree

--- 250301/두 선분/two-lines.js
+++ 250301/두 선분/two-lines.js
@@ -1,13 +1,9 @@
 // x1, x2가 x3, x4에 포함되면서 더 짧은 선분일 경우도 있으니 네개 다 확인해야 함
+// (!) 겹치는 조건이 아니라 떨어지는 조건 보기
 const fs = require("fs");
 const input = fs.readFileSync(0).toString().trim().split('\n');
 const [x1, x2, x3, x4] = input[0].split(' ').map(Number);
 
-function check() {
-    if (x1 <= x3 && x3 <= x2) return true
-    if (x1 <= x4 && x4 <= x2) return true
-    if (x3 <= x1 && x1 <= x4) return true
-    if (x3 <= x2 && x2 <= x4) return true
-    return false
-}
-console.log(check()? 'intersecting' : 'nonintersecting')
+let flag = true
+if (x2 < x3 || x4 < x1) flag = false
+console.log(flag? 'intersecting' : 'nonintersecting')
