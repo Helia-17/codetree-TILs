8c25e7fdf11a854520fa5bc6e60f78279437ab06
김지윤(Jiyun Kim) <76979903+Helia-17@users.noreply.github.com>
2025-03-01 17:47:41 +0900

    Title: 전부 겹치는 선분 2, Time: 202ms, Memory: 15MB, Status: Passed - CodeTree

--- 250301/전부 겹치는 선분 2/overlapping-line-segments-2.js
+++ 250301/전부 겹치는 선분 2/overlapping-line-segments-2.js
@@ -1,3 +1,6 @@
+// (!) 안겹치는 선분을 제거하는 것으로는 동일한 두개의 선분이 다른 선분으로부터 떨어져 있는 경우를 잡을 수 없음
+// (!) 모두 서로서로 겹치는지가 아니라 모든 선분이 "한 점"에서 겹치는지를 체크해야
+// (!) 조건문 바꿀때는 신중하게 체크할 것
 const fs = require("fs");
 const input = fs.readFileSync(0).toString().trim().split('\n');
 
@@ -8,17 +11,22 @@ for (let i = 1; i <= n; i++) {
     x1List.push(x1);
     x2List.push(x2);
 }
-let cnt = 0
-for (let j = 0; j < n; j++) {
-    let inter = false
+let minX1 = Math.min(...x1List)
+let maxX2 = Math.max(...x2List)
+
+let flag = false
+for (let j = minX1; j < maxX2; j++) {
+    let cnt = 0
     for (let l = 0; l < n; l++) {
-        if (j === l) continue
-        if (x1List[j] <= x2List[l] || x2List[j] >= x1List[l]) {
-            inter = true
-            break
+        if (x1List[l] > j || j > x2List[l]) { // 안겹치는 선분
+            cnt += 1
+            if (cnt > 1) break
         }
     }
-    if (!inter) flag += 1
+    if (cnt <= 1) {
+        flag = true
+        break
+    }
 }
 
-console.log(cnt > 1 ? "No" : "Yes")
+console.log(flag? "Yes" : "No")
