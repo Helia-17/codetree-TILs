a1ffa2005a123639c5264264d6ed1f1d003f8712
김지윤(Jiyun Kim) <76979903+Helia-17@users.noreply.github.com>
2025-03-01 15:51:48 +0900

    Title: 최대 최소간의 차, Time: 188ms, Memory: 18MB, Status: Wrong Answer - CodeTree

---
+++ 250301/최대 최소간의 차/difference-between-maximum-and-minimum.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const [n, k] = input[0].split(' ').map(Number);
+const arr = input[1].trim().split(' ').map(Number);
+let minCos = Infinity
+let min = Math.min(...arr)
+
+while (min <= Math.max(...arr)) {
+    let cos = 0
+    for (let i=0; i<n; i++) {
+        cos += Math.min(Math.abs(arr[i]-min), Math.abs(arr[i]-min-k))
+    }
+    if (minCos > cos) minCos = cos
+    min += 1
+}
+
+console.log(minCos)
