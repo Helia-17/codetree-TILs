a8f9b7e973cd6bb65f0b09f01824f1770c680a04
김지윤(Jiyun Kim) <76979903+Helia-17@users.noreply.github.com>
2025-03-06 11:59:24 +0900

    Title: 비둘기와 전기줄, Time: 181ms, Memory: 15MB, Status: Passed - CodeTree

--- 250306/비둘기와 전기줄/pigeons-and-electric-cords.js
+++ 250306/비둘기와 전기줄/pigeons-and-electric-cords.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const input = fs.readFileSync(0).toString().trim().split('\n');
 const n = Number(input[0]);
 const movements = input.slice(1, n + 1).map(line => line.split(' ').map(Number));
-const arr = new Array(10).fill(-1)
+const arr = new Array(11).fill(-1)
 let cnt = 0
 for (let [idx, dir] of movements) {
   if (arr[idx] === -1) arr[idx] = dir
