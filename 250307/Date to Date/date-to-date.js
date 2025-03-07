72dbaa356d96a01aa6eb4f68e9914e35117c6ab9
김지윤(Jiyun Kim) <76979903+Helia-17@users.noreply.github.com>
2025-03-07 15:50:48 +0900

    Title: Date to Date, Time: 176ms, Memory: 15MB, Status: Passed - CodeTree

--- 250307/Date to Date/date-to-date.js
+++ 250307/Date to Date/date-to-date.js
@@ -1,9 +1,10 @@
 // (!) 당월 (am, bm) 은 합산하는 것이 아니라 ad, bd만 계산해야 한다
 // 따라서 slice시 당월 미포함이므로 end+1이아니라 end
+// (!) 날짜 계산 시 시작일을 포함하여 센다는 문구!
 const fs = require("fs");
 const [am, ad, bm, bd] = fs.readFileSync(0).toString().trim().split(' ').map(Number);
 const arr = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 const a = arr.slice(0, am).reduce((acc, cur)=>acc+cur,0) + ad
 const b = arr.slice(0, bm).reduce((acc, cur)=>acc+cur,0) + bd
 
-console.log(b - a)
+console.log(b - a + 1)
