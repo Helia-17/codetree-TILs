3c76494d92fccfc91d5f4ff564de41951bb7145c
김지윤(Jiyun Kim) <76979903+Helia-17@users.noreply.github.com>
2025-03-07 18:05:01 +0900

    Title: 최대로 겹치는 구간, Time: 181ms, Memory: 15MB, Status: Passed - CodeTree

--- 250307/최대로 겹치는 구간/maximum-overlapped-segments.js
+++ 250307/최대로 겹치는 구간/maximum-overlapped-segments.js
@@ -1,4 +1,5 @@
 // (!) 선분의 범위 (-100 ~ 100)에 주의
+// (!) 끝점에서 닿는 경우는 겹치는 것으로 생각하지 않으므로 시작점에 + 1
 const fs = require("fs");
 const input = fs.readFileSync(0).toString().trim().split('\n');
 
@@ -6,7 +7,7 @@ const n = Number(input[0]);
 const segments = input.slice(1).map(line => line.trim().split(' ').map(Number));
 const arr = new Array(200+1).fill(0)
 for (let [a, b] of segments) {
-  for (let i = a + 100; i <= b + 100; i++) {
+  for (let i = a + 100 + 1; i <= b + 100; i++) {
     arr[i] += 1
   }
 }
