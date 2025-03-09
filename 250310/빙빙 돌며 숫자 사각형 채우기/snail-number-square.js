0d0d7ac1aa366386913f73622552f8026593c1ea
김지윤(Jiyun Kim) <76979903+Helia-17@users.noreply.github.com>
2025-03-10 02:51:49 +0900

    Title: 빙빙 돌며 숫자 사각형 채우기, Time: 181ms, Memory: 15MB, Status: Passed - CodeTree

--- 250310/빙빙 돌며 숫자 사각형 채우기/snail-number-square.js
+++ 250310/빙빙 돌며 숫자 사각형 채우기/snail-number-square.js
@@ -3,8 +3,10 @@
 // 다음 스텝을 진행 후 방문했던 곳인지 확인하면 90% 틀더라도 돌릴 수가 없으므로 진행하기 전에 확인 필요
 // 벽은 현재 위치가 배열 안인지 볼 수도 있지만 다음 위치가 배열 밖인지를 볼 수도 있으므로 같은 기준으로 확인하려면 후자가 나음
 // (!) 조건 바꿀때 명제 확인하기
-// let isWall = [0, n-1].includes(r) && [0, n-1].includes(c) => &&였지만
-// let isWall = [-1, n].includes(nr) || [-1, n].includes(nc) => ||임
+// let isWall = [0, n-1].includes(r) && [0, m-1].includes(c) => &&였지만
+// let isWall = [-1, n].includes(nr) || [-1, m].includes(nc) => ||임
+// (!) 행 열 길이가 다를 수 있음에 주의, 변수를 n, m 달리 써야 함
+
 const fs = require("fs");
 const input = fs.readFileSync(0).toString().trim().split('\n');
 const [n, m] = input[0].split(' ').map(Number);
@@ -16,7 +18,7 @@ for (let i = 1; i <= n * m; i++) {
     arr[r][c] = i
     nr = r + dir[curDir][0]
     nc = c + dir[curDir][1]
-    let isWall = [-1, n].includes(nr) || [-1, n].includes(nc)
+    let isWall = [-1, n].includes(nr) || [-1, m].includes(nc)
     if (isWall || arr[nr][nc] !== 0) curDir = (curDir + 1) % 4
     r += dir[curDir][0]
     c += dir[curDir][1]
@@ -24,7 +26,7 @@ for (let i = 1; i <= n * m; i++) {
 
 for (let i = 0; i < n; i++) {
     let print = "";
-    for (let j = 0; j < n; j++) {
+    for (let j = 0; j < m; j++) {
         print += `${arr[i][j]} `;
     }
     console.log(print);
