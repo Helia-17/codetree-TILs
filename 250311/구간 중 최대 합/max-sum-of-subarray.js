43f11f0e5c8ce94058285c341aa4e7980701302c
김지윤(Jiyun Kim) <76979903+Helia-17@users.noreply.github.com>
2025-03-11 17:06:28 +0900

    Title: 구간 중 최대 합, Time: 179ms, Memory: 15MB, Status: Passed - CodeTree

--- 250311/구간 중 최대 합/max-sum-of-subarray.js
+++ 250311/구간 중 최대 합/max-sum-of-subarray.js
@@ -5,7 +5,8 @@ const [n, k] = input[0].split(' ').map(Number);
 const arr = input[1].split(' ').map(Number);
 let max = 0
 
-for (let i = 0; i < n-k; i++) {
+// (!) k가 1일 경우 모든 항목 탐색해야 함 -> i < n이 되어야 하므로 n - k + 1
+for (let i = 0; i < n-k+1; i++) {
     let total = 0
     for (let j = i; j < i+k; j++) {
         total += arr[j]
