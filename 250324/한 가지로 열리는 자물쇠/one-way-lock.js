ef28dc68ad955823baac56617d194d94f48d44d5
김지윤(Jiyun Kim) <jyk.helia@gmail.com>
2025-03-24 11:37:08 +0900

    Title: 한 가지로 열리는 자물쇠, Time: 173ms, Memory: 15MB, Status: Passed - CodeTree

--- 250324/한 가지로 열리는 자물쇠/one-way-lock.js
+++ 250324/한 가지로 열리는 자물쇠/one-way-lock.js
@@ -1,14 +1,15 @@
+// (!) cnt 0에서부터 세기 또는 모든 조합에서 열리지 않는 경우만 빼기
 const fs = require("fs");
 const input = fs.readFileSync(0).toString().trim().split('\n');
 const n = Number(input[0]);
 const [a, b, c] = input[1].trim().split(' ').map(Number);
 
-let cnt = 0
+let cnt = n * n * n
 for (let i = 1; i <= n; i++) {
   for (let j = 1; j <= n; j++) {
     for (let l = 1; l <= n; l++) {
-      if (Math.abs(a-i) <= 2 || Math.abs(b-j) <= 2 || Math.abs(c-l) <= 2) {
-        cnt += 1
+      if (Math.abs(a-i) > 2 && Math.abs(b-j) > 2 && Math.abs(c-l) > 2) {
+        cnt -= 1
       }
     }
   }
