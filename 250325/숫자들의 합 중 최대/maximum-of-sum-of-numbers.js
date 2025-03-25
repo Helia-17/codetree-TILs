8a1bb8ddc76c6e822307329decd1b19f00a25d1f
김지윤(Jiyun Kim) <jyk.helia@gmail.com>
2025-03-25 15:41:34 +0900

    Title: 숫자들의 합 중 최대, Time: 191ms, Memory: 15MB, Status: Passed - CodeTree

--- 250325/숫자들의 합 중 최대/maximum-of-sum-of-numbers.js
+++ 250325/숫자들의 합 중 최대/maximum-of-sum-of-numbers.js
@@ -4,7 +4,7 @@ const input = fs.readFileSync(0).toString().trim().split('\n');
 const [x, y] = input[0].split(' ').map(Number);
 let max = 0
 for (i=x; i<=y; i++) {
-  let sum = String(x).split('').map(Number).reduce((acc, cur) => acc + cur, 0)
+  let sum = String(i).split('').map(Number).reduce((acc, cur) => acc + cur, 0)
   if (max < sum) max = sum
 }
 console.log(max)
