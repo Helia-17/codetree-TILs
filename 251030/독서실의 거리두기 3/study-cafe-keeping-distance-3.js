f1022419536b3023e30b5a941c4336a612dfe378
김지윤(Jiyun Kim) <jyk.helia@gmail.com>
2025-10-30 17:04:37 +0900

    Title: 독서실의 거리두기 3, Time: 65ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 251030/독서실의 거리두기 3/study-cafe-keeping-distance-3.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const n = Number(input[0]);
+const seats = input[1].split("").map(Number);
+
+const dis = input[1].split(1).map(v => v.length)
+const max = Math.max(...dis)
+const updated = dis.map(v => (v === max) ? Math.ceil(max/2) : v)
+console.log(Math.max(...updated))
