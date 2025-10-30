fcbc544bbbebe7d163bb8ea9d379872378736071
김지윤(Jiyun Kim) <jyk.helia@gmail.com>
2025-10-30 17:11:10 +0900

    Title: 독서실의 거리두기 3, Time: 67ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 251030/독서실의 거리두기 3/study-cafe-keeping-distance-3.js
+++ 251030/독서실의 거리두기 3/study-cafe-keeping-distance-3.js
@@ -1,10 +1,11 @@
+//[!] 최소 거리 구하기
 const fs = require("fs");
 const input = fs.readFileSync(0).toString().trim().split('\n');
 
 const n = Number(input[0]);
 const seats = input[1].split("").map(Number);
 
-const dis = input[1].split(1).map(v => v.length)
+const dis = input[1].split(1).slice(1, -1).map(v => v.length)
 const max = Math.max(...dis)
 const updated = dis.map(v => (v === max) ? Math.ceil(max/2) : v)
-console.log(Math.max(...updated))
+console.log(Math.min(...updated))
